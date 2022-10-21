574abd6c866dcac1a28ba4509d42d0ac758c94b9
youngjin park <pyjs18593@gmail.com>
2022-10-21 17:40:15 +0900

    20221021 java

---
+++ JAVA1020/src/toString_/Test3.java
@@ -0,0 +1,78 @@
+package toString_;
+
+import java.util.Objects;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		// acc, acc2 인스턴스 생성
+		Account acc = new Account("111-1111-111", "홍길동", 10000);
+		Account acc2 = new Account("111-1111-111", "홍길동", 10000);
+		
+		// if문을 통해 두 인스턴스가 같은지 비교
+		if(acc.equals(acc2)) {
+			System.out.println("두 계좌는 동일한 계좌입니다!");
+		} else {
+			System.out.println("두 계좌는 다른 계좌입니다!");
+		}
+		
+		// 참조변수를 출력했을 때 => acc 의 계좌정보 : [계좌정보] 
+		System.out.println("acc의 계좌정보 : " + acc);
+		
+	}
+
+}
+
+/*
+ * Account 크래스 정의
+ * - 멤버변수
+ *   계좌번호(accountNo, 문자열)
+ *   예금주명(ownerName, 문자열)
+ *   현재잔고(balance, 정수)
+ * - 생성자 - 계좌번호, 예금주명, 현재잔고를 전달받아 초기화
+ * - equals() 메서드를 오버라이딩하여 계좌정보를 비교하도록 정의
+ * - toString() 메서드를 오버라이딩하여 계좌정보를 문자열로 리턴하도록 정의  
+ */
+
+class Account {
+	String accountNo;
+	String ownerName;
+	int balance;
+	
+	public Account(String accountNo, String ownerName, int balance) {
+		super();
+		this.accountNo = accountNo;
+		this.ownerName = ownerName;
+		this.balance = balance;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return Objects.equals(accountNo, other.accountNo) && balance == other.balance
+				&& Objects.equals(ownerName, other.ownerName);
+	}
+
+	@Override
+	public String toString() {
+		return "[accountNo=" + accountNo + ", ownerName=" + ownerName + ", balance=" + balance + "]";
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
---
+++ JAVA1020/src/wrapper_class/Ex.java
@@ -0,0 +1,41 @@
+package wrapper_class;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		/*
+		 * Wrapper 클래스
+		 * - 기본 데이터타입을 객체 형태로 관리해주는 클래스들의 모음
+		 *   => java.lang 패키지에 위치
+		 * - 기본 데이터타입과 1:1로 대응하는 8개의 클래스를 제공
+		 *   (Byte, Short, Integer, Long, Float, Double, Character, Boolean)
+		 * - Wrapper 클래스에서 제공하는 상수와 메서드 등을 활용하여
+		 *   기본 데이터타입 데이터를 다양하게 처리할 수 있음
+		 */
+		
+		System.out.println("byte 타입 메모리 크기(bit) : " + Byte.SIZE);
+		System.out.println("byte 타입 메모리 크기(byte) : " + Byte.BYTES);
+		System.out.println("byte 타입 최소값 : " + Byte.MIN_VALUE);
+		System.out.println("byte 타입 최대값 : " + Byte.MAX_VALUE);
+		
+		System.out.println("short 타입 메모리 크기(bit) : " + Short.SIZE);
+		System.out.println("short 타입 메모리 크기(byte) : " + Short.BYTES);
+		System.out.println("short 타입 최소값 : " + Short.MIN_VALUE);
+		System.out.println("short 타입 최대값 : " + Short.MAX_VALUE);
+		
+		System.out.println("char 타입 메모리 크기(bit) : " + Character.SIZE);
+		System.out.println("char 타입 메모리 크기(byte) : " + Character.BYTES);
+		System.out.println("char 타입 최소값 : " + Character.MIN_VALUE);
+		System.out.println("char 타입 최대값 : " + Character.MAX_VALUE);
+		
+
+	}
+
+}
+
+
+
+
+
+
+
